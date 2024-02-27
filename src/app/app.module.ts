202c14185d8397a201084da3979874ec3d9018f1
oshin_stha <oshinshrestha2055@gmail.com>
2024-02-27 17:27:16 +0545

    feat(app): add initial setup with Husky and translation support

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/app/environments/environment.ts
@@ -0,0 +1,11 @@
+export enum EnvironmentTs {}
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDWZxpNFJR1Fd7xY5hbeGhCP6Ekvn18MP8',
+  authDomain: 'hr-management-system-ac8ce.firebaseapp.com',
+  projectId: 'hr-management-system-ac8ce',
+  storageBucket: 'hr-management-system-ac8ce.appspot.com',
+  messagingSenderId: '89377056248',
+  appId: '1:89377056248:web:df83081825dbb559cca20d',
+  measurementId: 'G-80FEQ7WBF2',
+};

e741b521779c0f9ee287a365c744b926ddfa14d4
oshin_stha <oshinshrestha2055@gmail.com>
2024-03-04 11:29:34 +0545

    fix(login): error alert of login fixed

--- src/app/features/user/services/login-services/login.service.ts
+++ src/app/features/user/services/login-services/login.service.ts
@@ -2,36 +2,17 @@ import { Injectable } from '@angular/core';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { AUTH } from 'src/app/environments/environment';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LoginService {
- 
-  // logInUser(email: string, password: string): Observable<unknown> {
-  //   return of(signInWithEmailAndPassword(AUTH, email, password)).pipe(
-  //     catchError((error) => {
-  //       return throwError(() => this.getErrorMessage(error.code));
-  //     })
-  //   );
-  // }
-
   logInUser(email: string, password: string): Promise<unknown> {
-    return signInWithEmailAndPassword(AUTH, email, password)
-      .catch((error) => {
-        throw this.getErrorMessage(error.code);
-      });
+    return signInWithEmailAndPassword(AUTH, email, password).catch((error) => {
+      alert(error.code);
+      throw this.getErrorMessage(error.code);
+    });
   }
 
-getErrorMessage(message:string): string{
-  alert(message);
-  switch(message) {
-      case 'EMAIL_NOT_FOUND':
-          return 'Email Not Found.';
-      case 'INVALID_PASSWORD':
-          return 'Invalid Password.';
-      case 'EMAIL_EXISTS':
-          return 'Email Already Exists.';
-      default:
-          return 'Unknown Error Occurred Please Try Again.';
+  getErrorMessage(message: string): string {
+    return message;
   }
-}
 }
